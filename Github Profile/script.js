43a8d04c7630c44ed1b8e0b98e1eacb44782dd45
Ebinu-s <ebinusuneer2nd@gmail.com>
2021-01-31 20:57:47 +0530

    working search and profile

--- Github Profile/script.js
+++ Github Profile/script.js
@@ -1,36 +1,66 @@
 const APIURL = 'https://api.github.com/users/';
-
-getResponse('Ebinu-S'); //initialised with my repository
+const REPO = '?tab=repositories'; 
 
 const mainEl = document.getElementById('main');
+const formEl= document.getElementById('form');
+const searchEl= document.getElementById('search');
 
+getResponse('Ebinu-S'); //initialised with my repository
 
 async function getResponse(username) { 
     const resp = await fetch(APIURL + username);
     const respData = await resp.json();
+    renderProfile(respData);
+    getRepos(respData.login);
+}
 
+async function getRepos(username){
+    const resp = await fetch(APIURL+username+"/repos");
+    const respData = await resp.json();
     console.log(respData);
 }
 
-function renderProfile(profile) {
-    mainEl.innerHTML = `
-    <img src="https://avatars.githubusercontent.com/u/54814859?v=4" alt="avatar" class="avatar">
-        <div class="uInfo">
-            <h3>Ebinu Suneer</h3>
-            <p>Average programming Enjoyer.</p>
-            <div class="countData">
-                <span>Followers : 1232 </span>
-                <span>following : 123 </span>
-                <span>Repos : 12 </span>
-            </div>
-        </div>
-        <div class="btns">
-            <a href="https://github.com/Ebinu-S">Visit Profile</a>
-            <a href="https://github.com/Ebinu-S?tab=repositories">Repos</a>
-        </div>
 
-    `
-  };
+function renderProfile(profile) {
+    if(profile.message == "Not Found"){
+        mainEl.innerHTML = `<div class = 'notExist'> The Person you are searching for does not exist <i class="fas fa-heart-broken"></i> </P>`;
+    }
+    else{
+        mainEl.innerHTML = `
+        <img src="${profile.avatar_url}" alt="${profile.login}" class="avatar">
+            <div class="uInfo">
+                <h3>${profile.name}</h3>
+                <h5>${profile.login}</h5>
+                <p>${profile.bio}</p>
+                <div class="countData">
+                    <span>Followers : ${profile.followers} </span>
+                    <span>following : ${profile.following} </span>
+                    <span>Repos : ${profile.public_repos} </span>
+                </div>
+            </div>
+            <div class="btns">
+                <a href="${profile.html_url}" target='_blank'>Visit Profile</a>
+                <a href="${profile.html_url + REPO} " target='_blank'>Repos</a>
+            </div>
+    
+        `
+    }
+
+ };
+
+
+
+ formEl.addEventListener('submit', (e)=> {
+     e.preventDefault();    
+
+     const search = searchEl.value;
+
+     if(search){
+         getResponse(search);
+         searchEl.innerHTML ='';
+     }
+
+ });
   
 
 
