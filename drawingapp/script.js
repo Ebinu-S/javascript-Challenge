b9bb1f046a54952892b2a5d7437fd04e79a0c46a
Ebinu-s <ebinusuneer2nd@gmail.com>
2021-02-01 13:01:56 +0530

    drawing app complete

--- drawingapp/script.js
+++ drawingapp/script.js
@@ -1,5 +1,10 @@
 const contx = canvas.getContext('2d');
 const canvasEL = document.getElementById('canvas');
+const sizeI = document.getElementById('increment');
+const sizeD = document.getElementById('decrement');
+const sizeEl = document.getElementById('sizeEl');
+const clrBtn = document.getElementById('clr');
+const colorEl = document.getElementById('color');
 
 let isPressed = false;  //determines whether to draw or not
 let size = 10;
@@ -7,6 +12,7 @@ let x1 = undefined;
 let y1 = undefined;
 let x2 = undefined;
 let y2 = undefined;
+let color = '#000';
 
 canvasEL.addEventListener('mousedown', (e) => {
     isPressed = true;
@@ -31,23 +37,47 @@ canvasEL.addEventListener('mousemove', (e) => {
     }
 });
 
+//toolbar button
+
+sizeI.addEventListener('click', ()=>{
+    if(size < 30){
+        size += 2;
+    }
+    sizeUpdateEl();
+});
+
+sizeD.addEventListener('click', ()=>{
+    if ( size >= 4) {
+        size -= 2;
+    }
+    sizeUpdateEl();
+})
+
+colorEl.addEventListener('change', ()=> {
+    color = colorEl.value;
+})
+
+clrBtn.addEventListener('click', ()=> {
+    contx.clearRect(0,0,canvasEL.width, canvasEL.height);
+});
+
+
 function drawCirlce(x,y) { 
     contx.beginPath();
     contx.arc(x,y,size,0,2 * Math.PI);
-    contx.stroke();
+    contx.fillStyle = color;
+    contx.fill();
  };
 
- function drawLine(x1,y1,x2,y2) { 
+function drawLine(x1,y1,x2,y2) { 
      contx.beginPath();
      contx.moveTo(x1,y1);
      contx.lineTo(x2,y2);
-     contx.lineWidth = size;
+     contx.lineWidth = size*2;
+     contx.strokeStyle = color;
      contx.stroke();
   };
 
-
-//  mode 
-//  if tunner  draw only drawCirlce
-//     line    draw line
-//     normal draboth        
-//      line staright dont update x1 y2 like aradien
+function sizeUpdateEl(){
+    sizeEl.innerText = size;
+}
