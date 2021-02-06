36a9ccc9d79e1972b58c527b862cf28c2e393bb6
Ebinu-s <ebinusuneer2nd@gmail.com>
2021-02-06 21:07:11 +0530

    weathe app update

--- weather app/script.js
+++ weather app/script.js
@@ -1,1 +1,12 @@
+const APIURL = 'https://www.metaweather.com/api/';
+const GETLOC = 'location/search/?query=';
+
+async function getPromise(url){
+    const prom = await fetch('https://www.metaweather.com/api/location/search/?query=london', {mode:'no-cors'});
+    const data = await prom.json();
+
+    console.log(prom);
+};
+
+getPromise();
 document.body.classList.add('rain');
