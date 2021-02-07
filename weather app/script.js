d598b76ae0601416b20a2a364a70d0d72b7f9dca
Ebinu-s <ebinusuneer2nd@gmail.com>
2021-02-07 09:41:13 +0530

    working history

--- weather app/script.js
+++ weather app/script.js
@@ -19,12 +19,20 @@ const sunsetEl = document.getElementById('sunsetD');
 
 const formEl = document.getElementById('form');
 const searchEl = document.getElementById('search');
+const a1El = document.getElementById('a1');
+const a2El = document.getElementById('a2');
+const a3El = document.getElementById('a3');
+
+let hisoryTemp = '';
 
 async function getPromise(loc){
 
     const prom = await fetch(APIURL+GETLOC+loc+APPID+MYKEY);
     if(prom.statusText == 'OK'){
         const data = await prom.json();
+        if(loc != a1El.dataset.value && loc != a2El.dataset.value){
+            manageHistory(loc);
+        }
         renderResult(data);
     }
     else{
@@ -50,13 +58,13 @@ function renderResult(data){
     windEl.children[1].innerText = data.wind.speed + ' km/h';
     humidityEl.children[1].innerText = data.main.humidity + ' %';
     pressureEl.children[1].innerText = data.main.pressure + ' hPa';
-    feelsLikeEl.children[1].innerText = data.main.feels_like  + ' °C';
-    minEl.children[1].innerText = data.main.temp_min + ' °C';
-    maxEl.children[1].innerText = data.main.temp_max  + ' °C';
+    feelsLikeEl.children[1].innerText = KelvinToCelcius(data.main.feels_like).toFixed(0)  + ' °C';
+    minEl.children[1].innerText = KelvinToCelcius(data.main.temp_min).toFixed(0) + ' °C';
+    maxEl.children[1].innerText = KelvinToCelcius(data.main.temp_max).toFixed(0)  + ' °C';
     cloudsEl.children[1].innerText = data.clouds.all + ' %';
-    sunriseEl.children[1].innerText = sunriseTime.getHours() + ': '+ sunriseTime.getMinutes() + ' AM';
-    sunsetEl.children[1].innerText = (sunsetTime.getHours() % 12) + ': ' + sunsetTime.getMinutes() + ' PM';
-}
+    sunriseEl.children[1].innerText = sunriseTime.getHours() + ':'+ sunriseTime.getMinutes() + ' AM';
+    sunsetEl.children[1].innerText = (sunsetTime.getHours() % 12) + ':' + sunsetTime.getMinutes() + ' PM';
+};
 
 getPromise('kottayam');
 
@@ -67,11 +75,39 @@ formEl.addEventListener('submit',(e)=>{
 
 searchEl.addEventListener('blur', ()=>{
     getPromise(searchEl.value);
-})
+});
+
+a1El.addEventListener('click', ()=> {
+    getPromise(a1El.dataset.value);
+});
+
+a2El.addEventListener('click', ()=> {
+    getPromise(a2El.dataset.value);
+});
+
+a3El.addEventListener('click', ()=> {
+    getPromise(a3El.dataset.value);
+});
+
+//manages search history
+function manageHistory(search){
+    hisoryTemp = a1El.dataset.value;
+    a1El.dataset.value  = search;
+    a1El.innerText = a1El.dataset.value;
+    a3El.dataset.value = a2El.dataset.value;
+    a3El.innerText = a3El.dataset.value
+    a2El.dataset.value = hisoryTemp;
+    a2El.innerText = a2El.dataset.value
+};
 
 function KelvinToCelcius(temp){
     return temp - 273.15;
-}
+};
+
+function updateLs(){
+    const history = [];
+    
+};
 
 /*
 
@@ -81,7 +117,6 @@ function KelvinToCelcius(temp){
 ?   add option to delete history
 *   display error when data isnt present
 ?   Add the clock?
-*   clicking history should return value
 ?   add suggested
 ?   option to get location ?
 * 
