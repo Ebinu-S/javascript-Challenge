cdb8d039c176c2e3d45a26b6241716a8d4dc8c52
Ebinu-s <ebinusuneer2nd@gmail.com>
2021-02-07 08:59:01 +0530

    more details working form

--- weather app/script.js
+++ weather app/script.js
@@ -1,7 +1,6 @@
 const APIURL = 'http://api.openweathermap.org/data/2.5/';
 const GETLOC = 'weather?q=';
 const APPID = '&appid='
-//http://api.openweathermap.org/data/2.5/weather?q=London&appid=id
 
 const MYKEY = config.key;
 
@@ -11,30 +10,64 @@ const weatherEl = document.getElementById('weatherD');
 const windEl = document.getElementById('windD');
 const humidityEl = document.getElementById('HumidityD');
 const pressureEl = document.getElementById('pressureD');
+const feelsLikeEl = document.getElementById('feelsLikeD');
+const minEl = document.getElementById('minD');
+const maxEl = document.getElementById('maxD');
+const cloudsEl = document.getElementById('cloudsD');
+const sunriseEl = document.getElementById('sunriseD');
+const sunsetEl = document.getElementById('sunsetD');
 
-async function getPromise(url){
-    const prom = await fetch(APIURL+GETLOC+url+APPID+MYKEY);
-    const data = await prom.json();
+const formEl = document.getElementById('form');
+const searchEl = document.getElementById('search');
 
-    // console.log(data);
-    console.log(data);
-    renderResult(data);
+async function getPromise(loc){
+
+    const prom = await fetch(APIURL+GETLOC+loc+APPID+MYKEY);
+    if(prom.statusText == 'OK'){
+        const data = await prom.json();
+        renderResult(data);
+    }
+    else{
+        alert('invalid Input');
+        // todo style the error message
+    }
+
 };
 
 
 function renderResult(data){
+
+    document.body.classList.add('rain');
+    
+    const sunriseTime = new Date(data.sys.sunrise * 1000);
+    const sunsetTime = new Date(data.sys.sunset * 1000);
+
     currentTempEL.innerText = KelvinToCelcius(data.main.temp).toFixed(1) + ' °C';
     locNameEl.children[0].innerText = data.name;
     //  todo : get current time
     //  todo : change class of the i acording to the weather 
     weatherEl.children[1].innerText = data.weather[0].main;
     windEl.children[1].innerText = data.wind.speed + ' km/h';
-    humidityEl.children[1].innerText = data.main.humidity;
-    pressureEl.children[1].innerText = data.main.pressure;
+    humidityEl.children[1].innerText = data.main.humidity + ' %';
+    pressureEl.children[1].innerText = data.main.pressure + ' hPa';
+    feelsLikeEl.children[1].innerText = data.main.feels_like  + ' °C';
+    minEl.children[1].innerText = data.main.temp_min + ' °C';
+    maxEl.children[1].innerText = data.main.temp_max  + ' °C';
+    cloudsEl.children[1].innerText = data.clouds.all + ' %';
+    sunriseEl.children[1].innerText = sunriseTime.getHours() + ': '+ sunriseTime.getMinutes() + ' AM';
+    sunsetEl.children[1].innerText = (sunsetTime.getHours() % 12) + ': ' + sunsetTime.getMinutes() + ' PM';
 }
 
 getPromise('kottayam');
-document.body.classList.add('rain');
+
+formEl.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    getPromise(searchEl.value);
+});
+
+searchEl.addEventListener('blur', ()=>{
+    getPromise(searchEl.value);
+})
 
 function KelvinToCelcius(temp){
     return temp - 273.15;
@@ -46,9 +79,6 @@ function KelvinToCelcius(temp){
 *   Change the background according to the weather
 ?   store and retreive search history in local Storage
 ?   add option to delete history
-*   add more details about weather
-*   make form working LOL
-*   search on blur
 *   display error when data isnt present
 ?   Add the clock?
 *   clicking history should return value
