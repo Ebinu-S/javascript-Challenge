a63832042bfe2141013bb59b131664ebc6d7af3c
Ebinu-s <ebinusuneer2nd@gmail.com>
2021-02-10 21:01:33 +0530

    added themes

--- Analog clock/script.js
+++ Analog clock/script.js
@@ -2,6 +2,14 @@ const secondHand = document.querySelector('.second');
 const minuteHand = document.querySelector('.minute');
 const hourHand = document.querySelector('.hour');
 const digitalHr = document.querySelector('.digital');
+const clockEl = document.querySelector('.clock');
+const btnsEl = document.querySelector('.btns');
+const ballEl = document.querySelector('.ball');
+const numberEl = document.querySelectorAll('.number');
+
+const t1Btn = document.getElementById('t1');
+let themeIndex = 0;
+
 
 function clockMain() {
     let date = new Date();
@@ -34,3 +42,53 @@ function rotateHand(hand,angle){
 clockMain();
 
 setInterval(() => {clockMain();}, 1000);
+
+t1Btn.addEventListener('click', ()=> {
+    if(themeIndex == 0){
+        resetClasses();
+        console.log('yes');
+        document.body.classList.add('t1');
+        clockEl.classList.add('t1');
+        digitalHr.classList.add('t1');
+        secondHand.classList.add('t1');
+        minuteHand.classList.add('t1');
+        hourHand.classList.add('t1');
+        ballEl.classList.add('t1');
+        btnsEl.classList.add('t1');
+    }
+    else if(themeIndex == 1){
+        resetClasses();
+        document.body.classList.add('t2');
+        clockEl.classList.add('t2');
+        digitalHr.classList.add('t2');
+        secondHand.classList.add('t2');
+        minuteHand.classList.add('t2');
+        hourHand.classList.add('t2');
+        ballEl.classList.add('t2');
+        btnsEl.classList.add('t2');
+    }
+    else{
+        resetClasses();
+    }
+});
+
+//reset the classes to its default 
+//* must call before adding new class
+function resetClasses(){
+
+    document.body.className = '';
+    clockEl.className = 'clock';
+    digitalHr.className = 'digital';
+    ballEl.className = 'ball';
+    secondHand.className = 'second';
+    minuteHand.className = 'minute';
+    hourHand.className = 'hour';
+    btnsEl.className = 'btns';
+    
+    if(themeIndex < 2){
+        themeIndex +=1; 
+    }
+    else{
+        themeIndex = 0;
+    }
+} 
