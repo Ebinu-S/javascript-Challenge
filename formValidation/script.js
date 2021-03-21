451d8b5ba83c4adacede12273f46baa030ddc79b
Ebinu-s <ebinusuneer2nd@gmail.com>
2021-03-21 23:37:35 +0530

    form validation check done

---
+++ formValidation/script.js
@@ -0,0 +1,66 @@
+const form =  document.getElementById('form');
+const userName = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    //username
+    if (userName.value == '') {
+        returnMessage(userName,  `Username can't be empty`, false);
+    }
+    else{
+        returnMessage(userName ,'', true);
+    }
+
+    //email
+    if (email.value == '') {
+        returnMessage(email, `Email can't be empty.`, false);
+    }
+    else if(!isEmail(email.value)){
+        returnMessage(email, `Invalide Email.`, false);
+    }
+    else{
+        returnMessage(email, '', true);
+    }
+
+    // password
+    if (password.value == '') {
+        returnMessage(password, `Enter password`,false);
+    }
+    else if (password.value.length < 7){
+        returnMessage(password, `minimum password length is 7. `,false);
+    }
+    else{
+        returnMessage(password, ``,true);
+    }
+
+    //second password
+
+    if (password2.value == '') {
+        returnMessage(password2, `Enter password again.`,false);
+    }
+    else if (password2.value != password.value) {
+        returnMessage(password2, `Password dosen't match.`,false);
+    }
+    else{
+        returnMessage(password2,``,true);
+    }
+
+
+});
+
+function returnMessage(input, message, isSuccess){
+    const formControll = input.parentElement;
+    if(isSuccess){
+        formControll.className = 'form-control success';
+    }else{
+        formControll.className = 'form-control error';
+    }
+    formControll.querySelector('small').innerText = message;  
+};
+
+function isEmail(email) { 
+     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)};
--- script.js
+++ script.js
@@ -2,18 +2,16 @@ function fallAnim() {
     const fallobj = document.createElement('i');
     fallobj.classList.add("fas");
     fallobj.classList.add("fa-code");
-    fallobj.style.left = Math.random() * window.innerWidth + "px";
+    fallobj.style.left = Math.random() * window.innerWidth + "px";  
     var rvalue = Math.random(); // random value for size and opacity
     fallobj.style.fontSize = rvalue * 10 + 4 +'px';
     fallobj.style.opacity = rvalue - 0.4;
     fallobj.style.animationDuration = (rvalue - 1) * -1 * 20 + 14 +'s';
-
-
     document.body.appendChild(fallobj)
 
     setTimeout( () =>{
         fallobj.remove();
     },23200);
  }
-
- setInterval(fallAnim,700);
+fallAnim();
+setInterval(fallAnim,700);
